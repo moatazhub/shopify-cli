70ed9381a30fd9ff41aacec31e7ce64231de1a7f
moataz <omr.salah@gmail.com>
2021-09-11 15:47:08 +0200

    add resourcePicker to app-bridge

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,38 @@
-import { Heading, Page } from "@shopify/polaris";
+import { Page } from "@shopify/polaris";
+import { ResourcePicker} from "@shopify/app-bridge-react";
 
-const Index = () => (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
-  </Page>
-);
+class Index extends React.Component{
+  state = {open: false}
+  render(){
+    return(
+      <Page
+        title='Product selector'
+        primaryAction={{
+          content:'Select products',
+          onAction: ()=> this.setState({open: true})
+        }}
+      >
+        <ResourcePicker
+          resourceType='Product'
+          open={this.state.open}
+          onCancel= {()=> this.setState({open: false})}
+          onSelection= { (resources)=> this.handleSelection(resources)}
+        />
+      </Page>
+  
+
+    )
+  }
+
+  handleSelection = (resources)=> {
+    const idFromResources = resources.selection.map((product)=> product.id);
+    this.setState({open: false});
+    console.log(idFromResources);
+  }
+
+}
+    
+ 
+
 
 export default Index;
