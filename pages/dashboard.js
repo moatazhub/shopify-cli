6fe0222624b4e7bf9e0a55f5350947fbb510abb3
moataz <omr.salah@gmail.com>
2021-09-16 20:49:38 +0200

    add dashboard code

---
+++ pages/dashboard.js
@@ -0,0 +1,11 @@
+import {Page, Heading} from '@shopify/polaris';
+
+const Dashboard = () => (
+    <Page>
+        <Heading>
+            dashboard page
+        </Heading>
+    </Page>
+);
+
+export default Dashboard;
